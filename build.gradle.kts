26b6ad3f249c0a27ea1b967f40f42ee2f88b9b62
Oliwier Juncewicz <oli.junc@gmail.com>
2024-06-29 02:43:15 +0200

    Updated build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,11 +16,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     extra.apply {
-        set("nav_version", "2.5.3")
+        set("nav_version", "2.7.7")
+        set("room_version", "2.6.1")
     }
 }
 plugins {
-    id("com.android.application") version "8.1.1" apply false
-    id("com.android.library") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.android.application") version "8.1.4" apply false
+    id("com.android.library") version "8.1.4" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
